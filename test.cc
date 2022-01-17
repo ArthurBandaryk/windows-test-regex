5707a16343cacb1afa5b63495a9f14005a616487
Artur <arthur_bandaryk@mail.ru>
2022-01-17 23:00:45 +0300

    Update test.cc

--- test.cc
+++ test.cc
@@ -18,15 +18,34 @@ TEST(RegexTest, TestDeathOutput) {
     std::cerr << "abc" << std::endl;
     exit(1);
   };
+  
+  auto operation3 = []() {
+    std::cerr << "Usage: program [...]" << std::endl << std::endl;
+    std::cerr << "  --[no-]bar         help" << std::endl;
+    std::cerr << "  --baz=...          help" << std::endl;
+    std::cerr << "  -duration=...      help" << std::endl;
+    std::cerr << "  --foo=...          help" << std::endl;
+    std::cerr << "  --[no-]help        whether or not to display this help message" << std::endl;
+    exit(1);
+  };
 
   const std::string match_regex1 =
       "program: Failed while parsing and validating flags:"
-      "\?\n?\?\n?"
+      "\?\n\?\n"
       ". 'bar' must be true"
-      "\?\n?\?\n?"
+      "\?\n\?\n"
       ". 'baz' must be greater than 42";
 
-  const std::string match_regex2 = "abc\?\n?\?\n?abc";
+  const std::string match_regex2 = "abc\?\n\?\nabc";
+  
+  const std::string match_regex3 =
+      "Usage: program \\[\\.\\.\\.\\]"
+      "\?\n\?\n"
+      "  --\\[no-\\]bar         help\?\n"
+      "  --baz=\\.\\.\\.          help\?\n"
+      "  --duration=\\.\\.\\.     help\?\n"
+      "  --foo=\\.\\.\\.          help\?\n"
+      "  --\\[no-\\]help        whether or not to display this help message";
 
   EXPECT_DEATH(
       operation1(),
@@ -35,4 +54,8 @@ TEST(RegexTest, TestDeathOutput) {
   EXPECT_DEATH(
       operation2(),
       testing::ContainsRegex(match_regex2));
+  
+   EXPECT_DEATH(
+      operation3(),
+      testing::ContainsRegex(match_regex3));
 }
